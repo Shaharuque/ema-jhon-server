d45e815742ce6f9e77116de061566f6c5129744f
devshaik444 <mamin172025@bsces.uiu.ac.bd>
2022-05-20 14:53:44 +0600

    ema-server done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors=require('cors')
 const port=process.env.PORT||5000;
 require('dotenv').config()
 const app=express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion ,ObjectId } = require('mongodb');
 
 //middleware
 app.use(cors());
@@ -22,19 +22,51 @@ async function run(){
 
         //all the product can be seen after hitting 'localhost:5000/product' url/api (aita amader created api jeita client thekey fetch korley data pabo )
         app.get('/product',async(req,res)=>{
+
+            console.log('query',req.query) //req.query=>client side thekey req ar sathey jei query pathano hoisey seita pabo
+
+            const page=parseInt(req.query.page)
+            const size=parseInt(req.query.size)
             const query={}  //all the data will be gathered thats why query null rakha hoisey
+            
             const cursor=productCollection.find(query)   //DB thekey particular collection ar sob product find korey niye asha hocchey and 'localhost:5000/product' api tey req korley res hisbaey products pathabey
-            const products= await cursor.toArray() //cursor.limit(10).toArray()=> tahley UI tey 10ta product e show korbey
-            res.send(products)
+
+            let products;
+            if(page || size){
+                //skip ar concept
+                //page 0 --> skip: 0 get: 0-10(10): 
+                //page 1 --> skip: 1*10 get: 11-20(10):
+                //page 2 --> skip: 2*10 get: 21-30 (10):
+                //page 3 --> skip: 3*10 get: 21-30 (10):
+                products= await cursor.skip(page*size).limit(size).toArray()
+            }
+            else{
+                //page or size ar information na thakley sob gula product DB thekey niye ashbey amn kicho
+                products= await cursor.toArray() //cursor.limit(10).toArray()=> tahley UI tey 10ta product e show korbey 
+            }
+           
+            res.send(products) //je ai api call korbey takey response hisabey products data dibey
         })
 
         app.get('/productCount',async(req,res)=>{
             //jokhn sob data lagbey tokhn query null rakhbo
             const query={}
             const cursor=productCollection.find(query)
-            const count=await cursor.count()
+
+            const count=await productCollection.estimatedDocumentCount()//now if i hit the api/url:http://localhost:5000/productCount we will get 76 as total products in the products collection
             res.send({count})  // res.json(count) ar res.send({count}) both are same 
-            //now if i hit the api/url:http://localhost:5000/productCount we will get 76 as total products in the products collection
+            
+        })
+
+        //use post to get  products by ids(normal POST method ar thekey ekto alada)
+        app.post('/productByKeys',async(req,res)=>{
+            const keys = req.body;
+            const ids = keys.map(id => ObjectId(id));
+            const query = {_id: {$in: ids}}
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            console.log(keys);
+            res.send(products);
         })
     }
     finally{
